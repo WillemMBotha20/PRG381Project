b521b4d38f956956108f0044acc3bb1106acd592
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-02-27 14:29:56 +0200

    Added deco list

--- BusinessLogicLayer/Bookings.java
+++ BusinessLogicLayer/Bookings.java
@@ -1,6 +1,7 @@
 package BusinessLogicLayer;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Bookings
@@ -12,67 +13,77 @@ public class Bookings {
     public String[][] EventVenue = new String[1][2];    //To capture address and contact number
     public int[][] PeopleCount = new int[1][2];         //To capture adults and children counts
     public String[][] Food = new String[1][4];          //To capture adult and kids meals and drinks
-
-
+    public List<String> Decorations;
     
 
     public String getEventType()
     {
         return EventType;
-    }
+    };
 
     public void setEventType(String EventType)
     {
         this.EventType = EventType;
-    }
+    };
 
     public LocalDate getEventDate()
     {
         return EventDate;
-    }
+    };
 
     public void setEventDate(LocalDate EventDate)
     {
         this.EventDate = EventDate;
-    }
+    };
 
     public String[][] getEventVenue()
     {
         return EventVenue;
-    }
+    };
 
     public void setEventVenue(String[][] EventVenue)
     {
         this.EventVenue = EventVenue;
-    }
+    };
 
     public int[][] getPeopleCount()
     {
         return PeopleCount;
-    }
+    };
 
     public void setPeopleCount(int[][] PeopleCount)
     {
         this.PeopleCount = PeopleCount;
-    }
+    };
 
     public String[][] getFood()
     {
         return Food;
-    }
+    };
 
     public void setFood(String[][] Food)
     {
         this.Food = Food;
-    }
+    };
 
-    public Bookings(String event,LocalDate date,String[][]venue,int[][] people,String[][] food)
+    public List<String> getDecorations()
+    {
+        return Decorations;
+    };
+
+    public void setDecorations(List<String> deco)
+    {
+        this.Decorations = deco;
+    };
+
+    public Bookings(String event,LocalDate date,String[][]venue,int[][] people,String[][] food,List<String> decorations)
     {
         this.EventType = event;
         this.EventDate = date;
         this.EventVenue = venue;
         this.PeopleCount = people;
         this.Food = food;
+        this.Decorations = decorations;
     };
     
 
