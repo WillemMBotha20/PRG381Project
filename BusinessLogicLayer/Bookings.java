49dbb69e16a44e65110ff975211a2221505bf9a9
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-02-27 12:06:46 +0200

    Added fields

--- BusinessLogicLayer/Bookings.java
+++ BusinessLogicLayer/Bookings.java
@@ -1,9 +1,18 @@
 package BusinessLogicLayer;
 
+import java.sql.Date;
+import java.util.*;
+
 /**
  * Bookings
  */
 public class Bookings {
 
-    //comment
+    public String Event;
+    public Date EventDate;
+    public String[][] EventVenue = new String[1][2];
+    public int[][] PeopleCount = new int[1][2];
+    public String[][] Food = new String[1][4];
+    
+
 }
