1070b22c11897efe07e2d1ea1f9dce8d407a3df2
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-02-27 12:43:06 +0200

    Changed Constructor

--- BusinessLogicLayer/Bookings.java
+++ BusinessLogicLayer/Bookings.java
@@ -9,7 +9,7 @@ import java.time.*;
  */
 public class Bookings {
 
-    public String Event;
+    public String EventType;
     public LocalDate EventDate;
     public String[][] EventVenue = new String[1][2];
     public int[][] PeopleCount = new int[1][2];
@@ -18,12 +18,63 @@ public class Bookings {
 
     public Bookings(String event,LocalDate date,String[][]venue,int[][] people,String[][] food)
     {
-        event = this.Event;
-        date = this.EventDate;
-        venue = this.EventVenue;
-        people = this.PeopleCount;
-        food = this.Food;
+        this.EventType = event;
+        this.EventDate = date;
+        this.EventVenue = venue;
+        this.PeopleCount = people;
+        this.Food = food;
     };
 
+    public String getEventType()
+    {
+        return EventType;
+    }
+
+    public void setEventType(String EventType)
+    {
+        this.EventType = EventType;
+    }
+
+    public LocalDate getEventDate()
+    {
+        return EventDate;
+    }
+
+    public void setEventDate(LocalDate EventDate)
+    {
+        this.EventDate = EventDate;
+    }
+
+    public String[][] getEventVenue()
+    {
+        return EventVenue;
+    }
+
+    public void setEventVenue(String[][] EventVenue)
+    {
+        this.EventVenue = EventVenue;
+    }
+
+    public int[][] getPeopleCount()
+    {
+        return PeopleCount;
+    }
+
+    public void setPeopleCount(int[][] PeopleCount)
+    {
+        this.PeopleCount = PeopleCount;
+    }
+
+    public String[][] getFood()
+    {
+        return Food;
+    }
+
+    public void setFood(String[][] Food)
+    {
+        this.Food = Food;
+    }
+
+    
 
 }
