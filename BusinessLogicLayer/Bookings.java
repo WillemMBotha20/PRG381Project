0d3bae4127f70b4d836a0b05d6f37811b55eb0b4
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-02-27 12:27:32 +0200

    Added constructor

--- BusinessLogicLayer/Bookings.java
+++ BusinessLogicLayer/Bookings.java
@@ -2,6 +2,7 @@ package BusinessLogicLayer;
 
 import java.sql.Date;
 import java.util.*;
+import java.time.*;
 
 /**
  * Bookings
@@ -9,10 +10,20 @@ import java.util.*;
 public class Bookings {
 
     public String Event;
-    public Date EventDate;
+    public LocalDate EventDate;
     public String[][] EventVenue = new String[1][2];
     public int[][] PeopleCount = new int[1][2];
     public String[][] Food = new String[1][4];
-    
+
+
+    public Bookings(String event,LocalDate date,String[][]venue,int[][] people,String[][] food)
+    {
+        event = this.Event;
+        date = this.EventDate;
+        venue = this.EventVenue;
+        people = this.PeopleCount;
+        food = this.Food;
+    };
+
 
 }
