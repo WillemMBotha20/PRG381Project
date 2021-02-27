4415b1a44a431159fecc8f0ffac3f02c40789970
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-02-27 19:15:38 +0200

    Fixed fileReadBooking(2)

--- DataAccesLayer/FileHandeler.java
+++ DataAccesLayer/FileHandeler.java
@@ -1,34 +1,36 @@
 package DataAccesLayer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 import BusinessLogicLayer.Bookings;
 
 public class FileHandeler {
     private File fileLocal;
-    private Bookings dataStorage;   
+    private Bookings dataStorage;
 
-    public FileHandeler(File fileLocal,Bookings dataStorage){
-     this.fileLocal = fileLocal; 
-     this.dataStorage = dataStorage;  
+    public FileHandeler(File fileLocal, Bookings dataStorage) {
+        this.fileLocal = fileLocal;
+        this.dataStorage = dataStorage;
     }
 
-    public FileHandeler(){
+    public FileHandeler() {
+    }
+
+    public void fileWriteBookings() {
 
     }
 
-    public void fileWriteBookings(){
-        
-    }
-
-    public List<Bookings> fileReadBooking(){
-        List<Bookings> returnBookings = new List<Bookings>();
+    public List<Bookings> fileReadBooking() throws FileNotFoundException {
+        List<Bookings> returnBookings = new ArrayList<Bookings>();
         Scanner sc = new Scanner(fileLocal);
 
         while(sc.hasNextLine()){
             returnBookings.add(new Bookings());
         }
+
+        sc.close();
 
         return returnBookings;
     }
