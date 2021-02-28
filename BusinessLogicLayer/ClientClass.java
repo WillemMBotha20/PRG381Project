e7e0176e1eeea6b9abd2ba57ee91be99ccea1b86
TimothyCloete99 <0799444080+TimothyCloete99@users.noreply.github.com>
2021-02-28 14:17:36 +0200

    Timothy commit

--- BusinessLogicLayer/ClientClass.java
+++ BusinessLogicLayer/ClientClass.java
@@ -1,4 +1,28 @@
 
 public class ClientClass {
-    //Push 1.1
+    private string Cname;
+    private string Csurname;
+    private int Cnumber;
+
+    public string getCname(){
+        return Cname;
+    }
+
+    public string getCsurname(){
+        return Csurname;
+    }
+
+    public int getCnumber(){
+        return Cnumber;
+    }
+    public  Client(){
+        
+    }
+    public Client(String Cname , String Csurname, int Cnumber)
+	{
+		this.Cname = Cname;
+		this.Csurname = Csurname;
+		this.Cnumber = Cnumber;
+	}
+
 }
