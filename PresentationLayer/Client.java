39c0642db8c090b030326413fc99dfee1f9b6d02
Willem Botha <sniperkeep14@gmail.com>
2021-03-06 10:59:56 +0200

    Started to work on the Pres Layer

--- PresentationLayer/Client.java
+++ PresentationLayer/Client.java
@@ -1,9 +1,49 @@
 package PresentationLayer;
 
+import java.util.Scanner;
+
 /**
  * Client This Is Just Temporary
  */
 public class Client {
-
     
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);        
+        boolean loopcontroll = true;    
+
+        // Creating the loop that 
+
+        while(loopcontroll == true) {
+            
+            switch (Integer.parseInt(scan.nextLine())) {
+                case 0:
+                    
+                    break;
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;  
+                case 6:
+                    
+                    break; 
+                case 7:
+                    
+                    break;            
+             
+            }
+
+        }    
+    }    
 }
